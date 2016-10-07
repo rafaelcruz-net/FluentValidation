60adfd1306c70e41b1ea04d175645ee605f7f10a
rafaelcruz-net <rafaelcruz_net@yahoo.com.br>
2016-10-07 12:57:36 -0300

    Initial Commit

---
+++ FluentValidation/FluentValidation/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Attributes;
+using FluentValidationSample.Validator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FluentValidationSample.Models
+{
+    [Validator(typeof(ProductValidator))]
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
---
+++ FluentValidation/FluentValidation/Validator/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidationSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FluentValidationSample.Validator
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            this.RuleFor(x => x.Name).NotEmpty().WithMessage("Nome é obrigatório")
+                .Length(1, 40).WithMessage("Nome não pode ser maior que 40 caracteres");
+
+            this.RuleFor(x => x.Description).NotEmpty().WithMessage("Descrição é obrigatório")
+                .Length(1, 255).WithMessage("Descrição não pode ser maior que 255 caracteres");
+
+            this.RuleFor(x => x.Price).NotEmpty().WithMessage("Preço é obrigatório")
+                .GreaterThan(0.0M).WithMessage("Preço do produto deve ser maior que zero");
+
+
+        }
+    }
+}
